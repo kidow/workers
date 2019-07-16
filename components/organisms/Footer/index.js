0bdae5643f597d904635cf3d6f1934c81cda0060
kidow <wcgo2ling@hanmail.net>
2019-07-17 02:11:39 +0900

    Footer

--- components/organisms/Footer/index.js
+++ components/organisms/Footer/index.js
@@ -1,3 +1,30 @@
+import Link from 'next/link'
+import './index.scss'
+
 export default () => {
-  return <footer>footer</footer>
+  return (
+    <footer>
+      <ul>
+        <li>
+          <Link href="/terms">이용약관</Link>
+        </li>
+        <li>
+          <span className="separator">•</span>
+          <Link href="/privacy">개인정보처리방침</Link>
+        </li>
+        <li>
+          <span className="separator">•</span>
+          <Link href="/about">소개</Link>
+        </li>
+        <li>
+          <span className="separator">•</span>
+          <Link href="/ads">광고문의</Link>
+        </li>
+        <li>
+          <span className="separator">•</span>
+          <a>고객센터</a>
+        </li>
+      </ul>
+    </footer>
+  )
 }
--- components/organisms/Header/index.js
+++ components/organisms/Header/index.js
@@ -1,3 +1,10 @@
+import './index.scss'
+import { Logo } from 'components/atoms'
+
 export default () => {
-  return <header>header</header>
+  return (
+    <header>
+      <Logo />
+    </header>
+  )
 }
